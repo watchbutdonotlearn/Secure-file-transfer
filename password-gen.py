42c96441e2861d34d905741a5e8e894d8272ca70
Anthony Xu <tonyxu@MacBook-Pro.local>
2020-10-07 14:38:45 -0500

    Utitilized Python json library

--- password-gen.py
+++ password-gen.py
@@ -1,6 +1,7 @@
 from random import choice
+from json import dump
 
-password_list = []
+password_dict = {}
 forbidden = ["\"", "\\"] # " and \ aren't allowed (special characters)
 ascii_range = [chr(i) for i in range(33, 127) if chr(i) not in forbidden] # Get all valid ASCII characters
 
@@ -8,13 +9,7 @@ for password_num in range(100):
     s = ""
     for char_num in range(156):
         s = s + choice(ascii_range) # Generate random ASCII character
-    password_list.append(s)
+    password_dict[str(password_num)] = s
 
 with open("passwords.json", "w") as f:
-    f.write("{\n")
-    for i in range(100):
-        f.write("\t\"" + str(i) + "\": \"<~" + password_list[i] + "~>\"") # Write password to json file
-        if i != 99:
-            f.write(",")
-        f.write("\n")
-    f.write("}\n")
+    dump(password_dict, f, indent=2) # Write password to json file
