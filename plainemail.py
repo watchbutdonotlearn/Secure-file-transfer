996abb60fc2253345c61c025a16e424e43ae83a4
Anthony Xu <tonyxu@MacBook-Pro.local>
2020-10-18 12:36:40 -0500

    Added passwords to encryption

--- plainemail.py
+++ plainemail.py
@@ -1,27 +1,31 @@
 import email, smtplib, ssl
-
 import pyzipper
-
+import json
+import random
 from email import encoders
 from email.mime.base import MIMEBase
 from email.mime.multipart import MIMEMultipart
 from email.mime.text import MIMEText
 
-
 def encrypt():
-    password = b'secret'
-    
+    passfilename = input("Input the name of the password file here: ")
+    passnum = random.randint(0, 99)
+    with open(passfilename, 'r') as f:
+        password_dict = json.load(f)
+    print("Password number " + str(passnum) + " is " + password_dict[str(passnum)])
+    passn = str(passnum) + ".zip"
+    passw = password_dict[str(passnum)].encode()
+    password = passw
     filetosend = input("Input the name of the file you want to send: ")
-    
-    with pyzipper.AESZipFile('newtest.zip',
+    with pyzipper.AESZipFile(passn,
             'w',
             compression=pyzipper.ZIP_LZMA,
             encryption=pyzipper.WZ_AES) as zf:
         zf.setpassword(password)
         zf.write(filetosend)
+    return passn
 
-
- # send the email
+# send the email
 def sendemail():
     subject = input("Type subject here: ")
     body = input("Type body text here: ")
@@ -39,11 +43,7 @@ def sendemail():
     # Add body to email
     message.attach(MIMEText(body, "plain"))
     
-    encrypt()
-    
-    
-    # filename = "a.txt"  # In same directory as script
-    filename = "newtest.zip"
+    filename = encrypt()
     
     with open(filename, "rb") as attachment:
         # Add file as application/octet-stream
@@ -71,15 +71,21 @@ def sendemail():
         server.sendmail(sender_email, receiver_email, text)
 
 def decrypt():
-    archivename = input("Input the name of the archive you want to decrypt: ")
-    password1 = input("TEMPORARY Input password name: ").encode() # FIX THIS LATER ON AND TAKE PASSWORDS FROM JSON FILE
-    fname = input("TEMPORARY Input filename inside of archive: ")
+    archivename = input('Input the name of the archive you want to decrypt: ')
+    passfilename = input('Input the name of the password file here: ')
+    #passnum = input('TEMPORARY input the password number here:') # FIX THIS AND MAKE IT READ PASS NUMBER FROM NAME OF FILE
+    passnum = archivename.strip(".zip")
+    with open(passfilename, "r") as f:
+        password_dict = json.load(f)
+    print("Password number " + str(passnum) + " is " + password_dict[str(passnum)])
+    passn2 = str(passnum)
+    passw2 = password_dict[str(passnum)].encode()
+    password1 = passw2
     with pyzipper.AESZipFile(archivename) as f:
         f.pwd = password1
         print(f.infolist())
-        filecontent = f.read(fname)
-        f.open(fname)
-        f.extractall(fname)
+        print(f.filename)
+        f.extractall(".")
 
 while True:
     selector = int(input("Type 1 for sending emails, type 2 for recieving emails, type 3 to exit: "))
@@ -93,4 +99,4 @@ while True:
     if selector == 3:
         break
 
-print("Thanks for using the Secure File Transfer.")
+print("Thanks for using the Secure File Transfer!")
